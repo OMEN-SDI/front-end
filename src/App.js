7d736545f7b24d9374c39f61480b91c013d81189
markscarna <markscarna@gmail.com>
2022-09-27 10:08:57 -0500

    basic boilerplate 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Login } from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
