5fadb958c9eaace9f9b270daff57a875b3d25a38
IGardocki <ian.d.gardocki@gmail.com>
2022-09-28 00:59:08 -0500

    minor search results changes

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -15,8 +15,8 @@ const ContainerDiv = Styled.div`
 export const SearchResults = () => {
     const dummyMissions =
         [
-            { title: "mission-one", msn_lat: "1.22324", msn_lon: "3.432" },
-            { title: "mission-two", msn_lat: "151", msn_lon: "142" },
+            { title: "mission-one", msn_lat: "40", msn_lon: "100" },
+            { title: "mission-two", msn_lat: "60", msn_lon: "99" },
             { title: "mission-three", msn_lat: "29.473676814427698", msn_lon: "-98.35372924804688" }
         ]
 
@@ -27,11 +27,11 @@ export const SearchResults = () => {
                     <Card style={{ width: "60%", height: "20%" }}>
                         <Card.Header>{mission.title}</Card.Header>
                         <Card.Body>
-                            <div>
-                                <iframe width="325" height="280" frameborder="0" src={`https://www.bing.com/maps/embed?h=280&w=325&cp=29.473676814427698~-98.35372924804688&lvl=11&typ=s&sty=h&src=SHELL&FORM=MBEDV8`} scrolling="no">
+                            {/* <div> */}
+                                <iframe width="25%" height="60%" frameborder="0" src={`https://www.bing.com/maps/embed?h=280&w=325&cp=${mission.msn_lat}~${mission.msn_lon}&lvl=11&typ=s&sty=h&src=SHELL&FORM=MBEDV8`} scrolling="no">
                                 </iframe>
                                 {/* <div style="white-space: nowrap; text-align: center; width: 325px; padding: 6px 0;"></div> */}
-                            </div>
+                            {/* </div> */}
                             <Card.Title>Special title treatment</Card.Title>
                             <Card.Text>
                                 With supporting text below as a natural lead-in to additional content.
