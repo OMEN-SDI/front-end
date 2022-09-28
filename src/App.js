ad750a5cefca67c229c8d5212a0cd404f24cb63a
markscarna <markscarna@gmail.com>
2022-09-28 16:55:16 -0500

    updated paths

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { UserPage } from "./components/UserPage";
 import { SearchResults } from "./components/SearchResults";
 import { AppContext } from "./components/AppContext";
 import { MissionDetails } from "./components/MissionDetails";
-import {AboutPage} from "./components/AboutPage";
-import {HelpPage} from "./components/HelpPage";
+import { AboutPage } from "./components/AboutPage";
+import { HelpPage } from "./components/HelpPage";
 
 function App() {
   const [individualMissionDetails, setIndividualMissionDetails] = useState({});
@@ -131,7 +131,7 @@ function App() {
         <MissionNavBar />
         <Routes>
           <Route path="/" element={<Login />}></Route>
-          <Route path="/user" element={<UserPage />}></Route>
+          <Route path="/userpage" element={<UserPage />}></Route>
           <Route path="/help" element={<HelpPage />}></Route>
           <Route path="/about" element={<AboutPage />}></Route>
           {/* This is a temp path */}
