e34ecd535fe305252904c0e93ac9fdaf797b978c
IGardocki <ian.d.gardocki@gmail.com>
2022-10-07 17:31:31 -0500

    troubleshooting heroku

--- src/components/URL.js
+++ src/components/URL.js
@@ -1,7 +1,9 @@
 const url = {
     development: `http://localhost:8080`,
+    // development:`https://omen-database.herokuapp.com`,
     production: `https://omen-database.herokuapp.com`
 }[process.env.NODE_ENV || "development"]
 
+console.log('process.env:', process.env)
 export default url;
 
