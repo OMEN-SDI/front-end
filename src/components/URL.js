83f51b5c4a8a43aa169b777eff5ea0f647c86cce
IGardocki <ian.d.gardocki@gmail.com>
2022-10-07 17:48:21 -0500

    added REACT_APP_ENV to URL.js

--- src/components/URL.js
+++ src/components/URL.js
@@ -2,7 +2,7 @@ const url = {
     development: `http://localhost:8080`,
     // development:`https://omen-database.herokuapp.com`,
     production: `https://omen-database.herokuapp.com`
-}[process.env.APP_ENV || "development"]
+}[process.env.REACT_APP_ENV || "development"]
 
 console.log('process.env:', process.env)
 export default url;
