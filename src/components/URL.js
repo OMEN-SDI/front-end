3626a6403f158a194e1b4967f9de626990b10fa0
IGardocki <ian.d.gardocki@gmail.com>
2022-10-07 17:38:41 -0500

    injected APP_ENV in URL.js

--- src/components/URL.js
+++ src/components/URL.js
@@ -2,7 +2,7 @@ const url = {
     development: `http://localhost:8080`,
     // development:`https://omen-database.herokuapp.com`,
     production: `https://omen-database.herokuapp.com`
-}[process.env.NODE_ENV || "development"]
+}[process.env.APP_ENV || "development"]
 
 console.log('process.env:', process.env)
 export default url;
