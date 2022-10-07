430aeb409056e196b77cdbf18c15cee469c24e3c
IGardocki <ian.d.gardocki@gmail.com>
2022-10-07 10:39:03 -0500

    about page almost done

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -6,21 +6,25 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
+import joshPic from '../joshPic.png';
+import boPic from '../boPic.png';
+import markPic from '../markPic.png';
+import ianPic from '../ianPic.jpg';
+import githubLogo from '../githubLogo.png';
 
 const ContainerDiv = Styled.div`
 display: flex;
 flex-wrap: wrap;
 justify-content: space-around;
-height: 85vh;
+min-height: 81vh;
 flex-direction: row;
 row-gap: 5vh;
-
+margin-top: 2vh;
 `;
 
 const InputStyleDiv = Styled.div`
 display: flex;
 flex-direction: row;
-
 `;
 
 const InputColDiv = Styled.div`
@@ -29,39 +33,82 @@ flex-direction: column;
 margin-left: 5%;
 `;
 
+const CeoPic = Styled.img`
+height: 15vh;
+width: 15vh;
+`
+
+const TitleDiv = Styled.div`
+    width: 97vw;
+    height: 15vh;
+    border: 4px solid white;
+    border-radius: 20px;
+    margin-top: 2vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    opacity: 80%;
+    font-size: 5vh;
+    font-weight: 500;
+    font-style: italic;
+    color: white;
+`;
+
+const FooterDiv = Styled.div`
+    width: 97vw;
+    height: 15vh;
+    border: 4px solid white;
+    border-radius: 20px;
+    margin-top: 2vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    opacity: 80%;
+    font-size: 5vh;
+    font-weight: 500;
+    font-style: italic;
+    color: white;
+`;
+
 const ceos = [
     {
         name: 'Joshua Clodfelter',
+        // jobTitle: 
         about: 'Software Developer',
-        image: 'imagehere'
+        image: joshPic
     },
     {
         name: 'Bo Bodenbender',
         about: 'Software Developer',
-        image: 'imagehere'
+        image: boPic
     },
     {
         name: 'Mark Scarna',
         about: 'Software Developer',
-        image: 'imagehere'
+        image: markPic
     },
     {
         name: 'Ian Gardocki',
         about: 'Software Developer and Bioengineer',
-        image: 'imagehere'
+        image: ianPic
     },
 ]
 
 export const AboutPage = () => {
     return (
+        <>
+        <TitleDiv>The Omen Team</TitleDiv>
         <ContainerDiv>
-
-            {/* <div> This is the about page. Bo has a cool mustache.</div> */}
+           
             {ceos.map((ceo) => {
                 return (
+                    
                     <Card
+                    // Match card style to mission cards
                         className="bg-secondary text-white"
-                        style={{ width: "40%", height: "23%", cursor: "pointer" }}
+                        style={{ width: "40vw", height: "23vh", cursor: "pointer" }}
                     >
                         <Card.Header className="bg-dark text-white">
                             {ceo.name}
@@ -69,7 +116,7 @@ export const AboutPage = () => {
                         <Card.Body className="show-grid">
                             <Container>
                                 <InputStyleDiv>
-                                <img src={ceo.image} alt={ceo.image}/>
+                                <CeoPic src={ceo.image} alt={ceo.image}/>
                                  
                                     <InputColDiv>
                                         <Card.Title>{ceo.about}</Card.Title>
@@ -86,5 +133,9 @@ export const AboutPage = () => {
                 );
             })}
         </ContainerDiv>
+        
+        {/* Get footer template online */}
+        <FooterDiv><CeoPic src={githubLogo}/></FooterDiv>
+        </>
     )
 }
